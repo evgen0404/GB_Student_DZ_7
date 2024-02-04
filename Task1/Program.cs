6217a92b1967b9825e212215459856162e7d66b1
evgen <evgen0404@mail.ru>
2024-02-04 15:48:33 +0300

    add Lesson7:Task2

---
+++ Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы. 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение M:");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение N:");
+        int n = int.Parse(Console.ReadLine());
+
+        if (m <= n)
+        {
+            
+            PrintNaturalNumbers(m, n);
+        }
+    }
+
+    static void PrintNaturalNumbers(int current, int end)
+    {
+        if (current <= end)
+        {
+            Console.Write(current);
+            
+            if (current < end)
+            {
+                Console.Write(", ");
+            }
+            
+            PrintNaturalNumbers(current + 1, end);
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение m:");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение n:");
+        int n = int.Parse(Console.ReadLine());
+
+        int result = AckermannFunction(m, n);
+
+        Console.WriteLine($"А({m},{n}): {result}");
+    }
+
+    static int AckermannFunction(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return AckermannFunction(m - 1, 1);
+        }
+        else
+        {
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+        }
+    }
+}
